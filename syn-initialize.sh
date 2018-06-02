a7a2453b8601cdee062ab488f791b475dcd2dda9
Brad Anderson <anderson.ab@gmail.com>
2018-06-02 19:28:06 -0400

    updated references

--- syn-initialize.sh
+++ syn-initialize.sh
@@ -6,12 +6,12 @@ docker run \
     --cap-add=NET_ADMIN \
 	--device=/dev/net/tun \
 	-d \
-	-v /volume1/foldername/resolv.conf:/etc/resolv.conf \
+	-v /volume1/foldername/syn-resolv.conf:/etc/resolv.conf \
 	-v /volume1/yourpath/:/data \
-	-e "OPENVPN_PROVIDER=PIA" \
-	-e "OPENVPN_CONFIG=Netherlands" \
-	-e "OPENVPN_USERNAME=XXXXX" \
-	-e "OPENVPN_PASSWORD=XXXXX" \
+	-e "OPENVPN_PROVIDER=${PROVIDER}" \
+	-e "OPENVPN_CONFIG=${CONFIG}" \
+	-e "OPENVPN_USERNAME=${USER}" \
+	-e "OPENVPN_PASSWORD=${PASSWD}" \
 	-e "LOCAL_NETWORK=192.168.0.0/24" \
 	-e "OPENVPN_OPTS=--inactive 3600 --ping 10 --ping-exit 60" \
 	-e "PGID=100" \
